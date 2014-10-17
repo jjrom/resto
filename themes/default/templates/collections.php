c85cfc4244571fcf6658144321716665a100905b
jerome.gasperi@gmail.com <>
2014-10-17 20:51:31 +0200

    Update /collections/ html page

--- themes/default/templates/collections.php
+++ themes/default/templates/collections.php
@@ -4,34 +4,42 @@
     <body class="glass">
         
         <!-- Header -->
+        <form id="resto-searchform" changeLocation="true" action="<?php echo $self->context->baseUrl . 'api/collections/search.html' ?>">
         <?php include 'header.php' ?>
+        </form>
+        
         <div class="row" style="height:100px;">
             <div class="large-12 columns"></div>
         </div>
         <div class="collections">
             <div class="row resto-collection fullWidth"> 
-                <ul class="small-block-grid-1 medium-block-grid-1 large-block-grid-3" id="_<?php echo $key;?>">
+                <ul class="small-block-grid-1 medium-block-grid-1 large-block-grid-3">
                     <?php foreach ($self->getCollections() as $key => $collection) { ?>
-                    <li class="collectionItem">
-                        <h1>
-                            <a class="fa" href="<?php echo $self->context->baseUrl . 'api/collections/' . $key . '/search.html?lang=' . $self->context->dictionary->language; ?>">  <?php echo $collection->osDescription[$self->context->dictionary->language]['ShortName']; ?></a><br/>
-                        </h1>
-                        <p><?php echo $collection->osDescription[$self->context->dictionary->language]['Description']; ?></p>
-                        <h1 class="center">
-                            <?php if ($self->context->user->canPut($key)) { ?><a class="fa fa-edit button bggreen updateCollection admin" href="#" collection="<?php echo $key; ?>" title="<?php echo $self->context->dictionary->translate('_update'); ?>"></a><?php } ?>
-                            <?php if ($self->context->user->canDelete($key)) { ?><a class="fa fa-trash-o button bgred removeCollection admin" href="#" collection="<?php echo $key; ?>" title="<?php echo $self->context->dictionary->translate('_remove'); ?>"></a><?php } ?>
-                        </h1>
+                    <li id="_<?php echo $key;?>">
+                        <div class="collectionItem">
+                            <h1 class="center">
+                                <a class="fa" href="<?php echo $self->context->baseUrl . 'api/collections/' . $key . '/search.html?lang=' . $self->context->dictionary->language; ?>">  <?php echo $collection->osDescription[$self->context->dictionary->language]['ShortName']; ?></a>
+                            </h1>
+                            <p><?php echo $collection->osDescription[$self->context->dictionary->language]['Description']; ?></p>
+                            <h1 class="center bottom">
+                                <?php if ($self->context->user->canPut($key)) { ?><a class="fa fa-edit button bggreen updateCollection admin" href="#" collection="<?php echo $key; ?>" title="<?php echo $self->context->dictionary->translate('_update'); ?>"></a><?php } ?>
+                                <?php if ($self->context->user->canDelete($key)) { ?><a class="fa fa-trash-o button bgred removeCollection admin" href="#" collection="<?php echo $key; ?>" title="<?php echo $self->context->dictionary->translate('_remove'); ?>"></a><?php } ?>
+                            </h1>
+                        </div>
+                    </li>
+                    <?php } ?>
+                    <?php if ($self->context->user->canPost()) { ?>
+                    <li>
+                        <div id="dropZone" class="_dropCollection collectionItem">
+                            <h1 class="center">
+                                <?php echo $self->context->dictionary->translate('_addCollection'); ?>
+                            </h1>
+                            <p class="center">+</p>
+                        </div>
                     </li>
                     <?php } ?>
                 </ul>
-            </div>
-            <?php if ($self->context->user->canPost()) { ?>
-            <div class="row resto-admin">
-                <div class="large-12 columns center">
-                    <div id="dropZone" class="_dropCollection"><h1><?php echo $self->context->dictionary->translate('_addCollection'); ?></h1><span class="fa fa-arrow-down"></span> <?php echo $self->context->dictionary->translate('_dropCollection'); ?> <span class="fa fa-arrow-down"></span></div>
-                </div>
-            </div>
-            <?php } ?>       
+            </div>     
         </div>
         
         <!-- Footer -->
@@ -75,6 +83,6 @@
             }
             
         </script>
-    <div align="center" ><img src="<?php echo $self->context->baseUrl ?>themes/<?php echo $self->context->config['theme'] ?>/img/bg.jpg" border="0" class="bg" ></div>
+        <div align="center" ><img src="<?php echo $self->context->baseUrl ?>themes/<?php echo $self->context->config['theme'] ?>/img/bg.jpg" border="0" class="bg" ></div>
     </body>
 </html>
--- themes/default/templates/home.php
+++ themes/default/templates/home.php
@@ -10,10 +10,10 @@
             <div class="large-12 columns"></div>
         </div>
         
-        <form id="resto-searchform-home" action="<?php echo $self->context->baseUrl . 'api/collections/search.html' ?>">
+        <form id="resto-searchform" changeLocation="true" action="<?php echo $self->context->baseUrl . 'api/collections/search.html' ?>">
         <div class="row fullWidth" style="width:100%;text-align:center;">
             <div class="large-12 columns">
-                <span class="resto-search"><input id="homesearch" class="darker" type="search" name="q" placeholder="<?php echo $self->context->dictionary->translate('_menu_search'); ?>" value="<?php echo isset($self->context->query['q']) ? $self->context->query['q'] : ''; ?>" style="font-size:1.5em;height:50px;width:500px;"></span>
+                <span class="resto-search"><input id="search" class="darker" type="text" name="q" placeholder="<?php echo $self->context->dictionary->translate('_menu_search'); ?>" value="<?php echo isset($self->context->query['q']) ? $self->context->query['q'] : ''; ?>" style="font-size:1.5em;height:50px;width:500px;"></span>
             </div>
         </div>
         </form>
@@ -31,17 +31,8 @@
                 ssoServices:<?php echo json_encode($self->context->config['ssoServices']) ?>,
                 userProfile:<?php echo json_encode(!isset($_SESSION['profile']) ? array('userid' => -1) : array_merge($_SESSION['profile'], array('rights' => isset($_SESSION['rights']) ? $_SESSION['rights'] : array()))) ?> 
             });
-            
-            $('#resto-searchform-home').submit(function(e) {
-                e.preventDefault();
-                R.showMask();
-                this.submit();
-            });
-            
-            $('#homesearch').focus();
         });
     </script>
     <div align="center" ><img src="<?php echo $self->context->baseUrl ?>themes/<?php echo $self->context->config['theme'] ?>/img/bg.jpg" border="0" class="bg" ></div>
     </body>
-    
 </html>
