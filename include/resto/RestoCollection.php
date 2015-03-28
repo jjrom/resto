dd814b3673074cde86738127f11e843050859219
jerome.gasperi@gmail.com <>
2015-03-28 11:25:33 +0100

    Creating collection was not working (!!) - corrected

--- include/resto/RestoCollection.php
+++ include/resto/RestoCollection.php
@@ -255,7 +255,7 @@ class RestoCollection {
         /*
          * Check JSON validity
          */
-        $this->checkJSONValidity();
+        $this->checkJSONValidity($object);
         
         /*
          * Set Model
