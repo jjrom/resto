fea09e9d6e567b2b3a76f8ba782a877e90fd322b
jerome.gasperi@cnes.fr <>
2015-05-04 09:24:29 +0200

    Close issue #95

--- _install/data/fr/generalKeywords.sql
+++ _install/data/fr/generalKeywords.sql
@@ -14,8 +14,8 @@ INSERT INTO resto.keywords (name, value, lang, type) VALUES ('radar', 'radar', '
 -- Location modifiers
 INSERT INTO resto.keywords (name, value, lang, type) VALUES ('côtier', 'coastal', 'fr', 'location');
 INSERT INTO resto.keywords (name, value, lang, type) VALUES ('côtière', 'coastal', 'fr', 'location');
-INSERT INTO resto.keywords (name, value, lang, type) VALUES ('litoral', 'coastal', 'fr', 'location');
-INSERT INTO resto.keywords (name, value, lang, type) VALUES ('litorale', 'coastal', 'fr', 'location');
+INSERT INTO resto.keywords (name, value, lang, type) VALUES ('littoral', 'coastal', 'fr', 'location');
+INSERT INTO resto.keywords (name, value, lang, type) VALUES ('littorale', 'coastal', 'fr', 'location');
 INSERT INTO resto.keywords (name, value, lang, type) VALUES ('hémisphere nord', 'northern', 'fr', 'location');
 INSERT INTO resto.keywords (name, value, lang, type) VALUES ('hémisphere sud', 'southern', 'fr', 'location');
 INSERT INTO resto.keywords (name, value, lang, type) VALUES ('zone equatoriale', 'equatorial', 'fr', 'location');
