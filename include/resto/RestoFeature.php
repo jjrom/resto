a492eb17a9233d069c4c32d908835222a851c95c
jjrom <jerome.gasperi@gmail.com>
2015-05-15 04:06:35 +0200

    Update issue #126

--- include/resto/RestoFeature.php
+++ include/resto/RestoFeature.php
@@ -221,13 +221,13 @@ class RestoFeature {
             * Optimized download with Apache module XsendFile
             */
             case 'apache':
-                return $this->streamApache($path);
+                return $this->streamApache($path, $mimetType);
                 
            /*
             * Optimized download with Apache module XsendFile
             */  
             case 'nginx':
-                return $this->streamNginx($path);
+                return $this->streamNginx($path, $mimetType);
             
            /*
             * Slower but generic PHP stream
