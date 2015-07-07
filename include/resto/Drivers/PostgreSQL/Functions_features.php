eab5e1c065bc74baa13a8cc5fe2b872a002c9928
jerome.gasperi@cnes.fr <>
2015-07-07 10:10:31 +0200

    Do not index keywords that have a non null cover lower than 10%

--- include/resto/Drivers/PostgreSQL/Functions_features.php
+++ include/resto/Drivers/PostgreSQL/Functions_features.php
@@ -350,12 +350,10 @@ class Functions_features {
         foreach (array_keys($keywords) as $hash) {
             
             /*
-             * Do not index location if cover is lower than 10 %
+             * Do not index keywords if cover is lower than 10 %
              */
-            if (in_array($keywords[$hash]['type'], array('country', 'region', 'state'))) {
-                if (!isset($keywords[$hash]['value']) || $keywords[$hash]['value'] < 10) {
-                    continue;
-                }
+            if (isset($keywords[$hash]['value']) && $keywords[$hash]['value'] < 10) {
+                continue;
             }
             $hashes[] = '"' . pg_escape_string($hash) . '"';
             $hashes[] = '"' . pg_escape_string($keywords[$hash]['type'] . ':' . (isset($keywords[$hash]['normalized']) ? $keywords[$hash]['normalized'] : strtolower($keywords[$hash]['name']))) . '"';
