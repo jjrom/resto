5349fe0e57d1ad245f79b1a9e6b410ff065d30c7
jjrom <jerome.gasperi@gmail.com>
2015-08-06 13:15:29 +0200

    [Security] avoid server response to be stored by client

--- include/resto/Resto.php
+++ include/resto/Resto.php
@@ -241,9 +241,11 @@ class Resto {
          * HTTP 1.1 headers
          */
         header('HTTP/1.1 ' . $responseStatus . ' ' . (isset(RestoLogUtil::$codes[$responseStatus]) ? RestoLogUtil::$codes[$responseStatus] : RestoLogUtil::$codes[200]));
-        header('Cache-Control:  no-cache');
+        header('Pragma: no-cache');
+        header('Cache-Control: no-cache, no-store, must-revalidate');
+        header('Expires: Fri, 1 Jan 2010 00:00:00 GMT');
         header('Content-Type: ' . RestoUtil::$contentTypes[$this->inError ? 'json' : $this->context->outputFormat]);
-        
+
         /*
          * Set headers including cross-origin resource sharing (CORS)
          * http://en.wikipedia.org/wiki/Cross-origin_resource_sharing
