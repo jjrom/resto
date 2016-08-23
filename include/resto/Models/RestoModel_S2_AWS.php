e5e63d77826244fdcb3bb22ce228c3033e863b2a
jjrom <jerome.gasperi@gmail.com>
2016-08-23 15:39:02 +0200

    Update S2_AWS model

--- include/resto/Models/RestoModel_S2_AWS.php
+++ include/resto/Models/RestoModel_S2_AWS.php
@@ -2,13 +2,13 @@
 
 /*
  * Copyright 2014 Jérôme Gasperi
- * 
+ *
  * Licensed under the Apache License, version 2.0 (the "License");
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -39,18 +39,28 @@ class RestoModel_S2_AWS extends RestoModel {
         'coverage' => array(
             'name' => 'coverage',
             'type' => 'NUMERIC'
+        ),
+        // Percentage of tile coverage (i.e. 100 if completely fill the tile)
+        'sciHubIngestion' => array(
+            'name' => 'scihubingestion',
+            'type' => 'TIMESTAMP'
+        ),
+        // Percentage of tile coverage (i.e. 100 if completely fill the tile)
+        's3Ingestion' => array(
+            'name' => 's3ingestion',
+            'type' => 'TIMESTAMP'
         )
     );
 
     /**
      * Constructor
-     * 
+     *
      * @param RestoContext $context : Resto context
      * @param RestoContext $user : Resto user
      */
     public function __construct() {
         parent::__construct();
-  
+
         $this->searchFilters['resto:dataTakeId'] = array(
             'key' => 'dataTakeId',
             'osKey' => 'dataTakeId',
