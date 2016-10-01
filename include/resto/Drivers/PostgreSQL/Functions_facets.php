c8f39e7849f5b2a577d24276ef1c8dbace863901
jjrom <jerome.gasperi@gmail.com>
2016-10-01 09:37:56 +0200

    Total count in statistics correctly computed

--- include/resto/Drivers/PostgreSQL/Functions_facets.php
+++ include/resto/Drivers/PostgreSQL/Functions_facets.php
@@ -279,7 +279,7 @@ class Functions_facets {
         }
 
         return array(
-            'count' => $collectionCount,
+            'count' => $count,
             'facets' => $facets
         );
     }
