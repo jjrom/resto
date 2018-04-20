d250efa4ec843995d7a1b1ba5dfe030cf461b926
jjrom <jerome.gasperi@gmail.com>
2018-04-20 11:04:23 +0200

    Add "-x" option to disable iTag during post

--- _scripts/postResource.sh
+++ _scripts/postResource.sh
@@ -16,8 +16,9 @@
 
 HTTPS=0
 HOST=localhost
+
 TARGET=resto
-usage="## Post a resource to collection\n\n  Usage $0 -c <Collection name> -f <Resource description file> -u <username:password> [-s (use https if set)  -H server (default localhost) -p resto path (default resto)]\n"
+usage="## Post a resource to collection\n\n  Usage $0 -c <Collection name> -f <Resource description file> -u <username:password> [-x (do not use iTag if set) -s (use https if set)  -H server (default localhost) -p resto path (default resto)]\n"
 while getopts "sf:c:u:p:hH:" options; do
     case $options in
         u ) AUTH=`echo $OPTARG`;;
@@ -25,6 +26,7 @@ while getopts "sf:c:u:p:hH:" options; do
         p ) TARGET=`echo $OPTARG`;;
         f ) FILE=`echo $OPTARG`;;
         s ) HTTPS=1;;
+        x ) NO_ITAG="?_useItag=0";;
         c ) COLLECTION=`echo $OPTARG`;;
         h ) echo -e $usage;;
         \? ) echo -e $usage
