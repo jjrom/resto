d60c66f42ce5214fa434691392077a7a01ca9652
jjrom <jerome.gasperi@gmail.com>
2018-05-18 13:07:24 +0200

    Update facets script

--- _scripts/updateFacets.php
+++ _scripts/updateFacets.php
@@ -3,15 +3,18 @@
 
 /*==========================================
  *
- * FOR cron task set these variable 
- * Otherwise use command line options
+ * FOR cron task $execute should be set to true
  *
  */
 $dbname = 'resto';
 $username = 'postgres';
 $password = 'postgres';
 $hostname = null;
+$from = 'NOW()';
+$timeframe = '1 DAY';
+$execute = false;
 $port = 5432;
+$orderColumn = 'updated';
 $nbOfFeaturesPerUpdate = 1000;
 /*=========================================*/
 
@@ -191,7 +194,7 @@ function getSQLQueries($counterArray) {
         );
 
         $insert = 'INSERT INTO resto.facets (uid, value, type, pid, collection, counter) SELECT ' . join(',', $arr);
-        $upsert = 'UPDATE resto.facets SET counter = ' .  $facetElement['counter'] .  '  WHERE uid = \'' . pg_escape_string($facetElement['uid']) . '\' AND collection = \'' . pg_escape_string($facetElement['collection']) . '\'';
+        $upsert = 'UPDATE resto.facets SET counter = counter + ' .  $facetElement['counter'] .  '  WHERE uid = \'' . pg_escape_string($facetElement['uid']) . '\' AND collection = \'' . pg_escape_string($facetElement['collection']) . '\'';
         
         $query = 'WITH upsert AS (' . $upsert . ' RETURNING *) ' . $insert . ' WHERE NOT EXISTS (SELECT * FROM upsert)';
         array_push($queries, $query);
@@ -205,15 +208,22 @@ function getSQLQueries($counterArray) {
 /*==========================================
  * Start of script
  *==========================================*/
-$help = "\nUpdate resto.facets table\n";
-$help .= "\n USAGE : updateFacets -u <user> -p <password> [-d <dbname> -H <hostname> -P <port>] \n";
-$help .= "   OPTIONS:\n";
+$help = "\nCompute UPSERT for resto.facets table\n";
+$help .= "\n Facets computation is done backward i.e. most recent features are processed first\n\n";
+$help .= "\n USAGE : updateFacets -u <user> -p <password> [OPTIONS] \n\n";
+$help .= "   MANDATORY:\n";
 $help .= "          -u <db user> : Database user name with update rights on resto database\n";
-$help .= "          -p <db password> : Database user password\n";
-$help .= "          -d <db name> : Database name (default resto)\n";
+$help .= "          -p <db password> : Database user password\n\n";
+$help .= "   OPTIONS:\n";
+$help .= "          -d <db name> : Database name (default " . $dbname . ")\n";
 $help .= "          -H <hostname> : make a TCP/IP connection (default is no host i.e. socket connection)\n";
-$help .= "          -P <port> : port for TCP/IP connection (default is 5432)\n";
-$options = getopt("u:p:d:H:P:h");
+$help .= "          -P <port> : port for TCP/IP connection (default is " . $port . ")\n";
+$help .= "          -o <orderColumn> : name of column used for ordering features, usually \"updated\" or \"published\" (default is " . $orderColumn . ") [WARNING]This column must be indexed[WARNING]\n";
+$help .= "          -n <nbOfFeaturesPerUpdate> : split whole process in block of \"nbOfFeaturesPerUpdate\" features (default is " . $nbOfFeaturesPerUpdate . ")\n";
+$help .= "          -f <from> : most recent \"" . $orderColumn . "\" date to start processing (default is " . $from . ")\n";
+$help .= "          -t <timeframe> : timeframe to stop processing based on from date (default is " . $timeframe . ")\n";
+$help .= "          -X : execute UPSERT on database (default is to stream UPSERT queries without execution)\n";
+$options = getopt("u:p:d:H:P:o:n:f:t:Xh");
 foreach ($options as $option => $value) {
     if ($option === "u") {
         $username = $value;
@@ -227,8 +237,23 @@ foreach ($options as $option => $value) {
     if ($option === "H") {
         $hostname = $value;
     }
-    if ($port === "P") {
+    if ($option === "P") {
         $port = $value;
+    }
+    if ($option === "o") {
+        $orderColumn = $value;
+    }
+    if ($option === "n") {
+        $nbOfFeaturesPerUpdate = $value;
+    }
+    if ($option === "f") {
+        $from = $value;
+    }
+    if ($option === "t") {
+        $timeframe = $value;
+    }
+    if ($option === "X") {
+        $execute = true;
     }
     if ($option === "h") {
         echo $help;
@@ -264,14 +289,16 @@ if (!isset($dbh)) {
 
 // Roll over each features
 $continue = true;
-$counterArray = array();
+$totalFeatures = 0;
+$totalQueries = 0;
+
 while ($continue) {
 
-    $lastPublished = isset($lastPublished) ? '\'' . $lastPublished . '\'' : 'now()';
-    $features = pg_query($dbh, 'SELECT id, collection, keywords, published FROM resto.features WHERE published < ' . $lastPublished . ' ORDER BY published DESC LIMIT ' . $nbOfFeaturesPerUpdate);
-    
+    $lastPublished = isset($lastPublished) ? $lastPublished : $from;
+    $features = pg_query($dbh, 'SELECT id, collection, keywords, ' . pg_escape_string($orderColumn) . ' FROM resto.features WHERE ' . pg_escape_string($orderColumn) . ' < ' . (is_numeric(substr($lastPublished, 0, 1)) ? '\'' . $lastPublished . '\'' : $lastPublished ) . ' AND ' . pg_escape_string($orderColumn) . ' > ' . (is_numeric(substr($from, 0, 1)) ? 'TO_TIMESTAMP(\'' . $from . '\', \'YYYY-MM-DD HH24:MI:SS\')' : $from ) . ' - INTERVAL \'' . $timeframe. '\' ORDER BY ' . pg_escape_string($orderColumn) . ' DESC LIMIT ' . pg_escape_string($nbOfFeaturesPerUpdate));
     $nbOfFeatures = 0;
     $published = null;
+    $counterArray = array();
 
     while ($feature = pg_fetch_assoc($features)) {
 
@@ -282,28 +309,50 @@ while ($continue) {
         $counterArray = updateCounter($counterArray, $facets, $feature['collection']);
 
         // Set published
-        $published = $feature['published'];
+        $published = $feature[$orderColumn];
 
         $nbOfFeatures++;
 
     }
 
-    // Some news
-    echo '[PROCESSED] ' . $nbOfFeatures . ' features from ' . $lastPublished . ' to ' . $published . "\n";
-
-    // Set lastPublished to exit the loop
-    $lastPublished = $published;
-    
     // Exit from the loop
     if ($nbOfFeatures === 0) {
         break;
     }
 
+    // Compute queries
+    $queries = getSQLQueries($counterArray);
+
+    // Execute queries...
+    if ( $execute ) {
+
+        for ($i = 0, $ii = count($queries); $i < $ii; $i++) {
+            pg_query($dbh, $queries[$i]);
+        }
+
+        echo '[FROM] ' . $lastPublished . ' to ' . $published . "\n";
+        echo '  ' . count($queries) . ' facets queries processed for ' . $nbOfFeatures . " features\n";
+
+    }
+    // ...or stream UPSERT to console
+    else {
+        for ($i = 0, $ii = count($queries); $i < $ii; $i++) {
+            echo $queries[$i] . ";\n";
+        }
+    }
+
+    // Total counters
+    $totalFeatures = $totalFeatures + $nbOfFeatures;
+    $totalQueries = $totalQueries + count($queries);
+     
+    // Set lastPublished to exit the loop
+    $lastPublished = $published;
+
 }
 
-$queries = getSQLQueries($counterArray);
-
-print_r($queries);
+if ( $execute ) {
+    echo "\n[END] " . $totalQueries . ' facets queries processed for ' . $totalFeatures . " features\n\n";
+}
 
 // Bye bye
 pg_close($dbh);
