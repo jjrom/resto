efc0f93cf31b1e046246eba67a8ef312334bf7c9
jjrom <jerome.gasperi@gmail.com>
2020-05-06 17:10:36 +0200

    Uses jjrom/nginx-fpm image as base image

--- build/resto/start-fpm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/with-contenv sh
-set -e;
-
-# Start PHP-FPM
-/usr/sbin/php-fpm${PHP_VERSION} -R --nodaemonize
--- build/resto/start-nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv sh
-set -e;
-
-# Use wait for it for TCP only
-#/bin/wait-for-it.sh -t 120 127.0.0.1:9000
-
-/usr/sbin/nginx -g "daemon off;"
