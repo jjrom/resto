b81e938349860199f5c6a86256abe801f71bb166
jjrom <jerome.gasperi@gmail.com>
2020-06-25 17:07:52 +0200

    Activate php preloading PHP_OPCACHE_ENABLE_PRELOADING in production only

--- build/resto/container_root/cont-init.d/09-php-opcache.sh
+++ build/resto/container_root/cont-init.d/09-php-opcache.sh
@@ -5,4 +5,9 @@ then
   echo '[debug] Opcache WATCHING for file changes'
 else
   echo '[debug] Opcache set to PERFORMANCE, NOT WATCHING for file changes'
+  if [[ $PHP_OPCACHE_ENABLE_PRELOADING = 1 || $PHP_OPCACHE_ENABLE_PRELOADING = '1' || $PHP_OPCACHE_ENABLE_PRELOADING = 'true' ]]
+  then
+    echo '[debug] Opcache enable preloading'
+    sed -i "s/;opcache.preload=/opcache.preload=/" /etc/php/${PHP_VERSION}/fpm/php.ini
+  fi
 fi
