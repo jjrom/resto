4201e6490d0c72b2774faea772b9358ebfc2c3da
jjrom <jerome.gasperi@gmail.com>
2020-06-25 16:57:39 +0200

    Enable opcache preloading by default

---
+++ build/resto/container_root/etc/fpm/preload.php
@@ -0,0 +1,34 @@
+<?php
+
+$phps = array();
+
+function loadPHP($folder)
+{
+    global $phps;
+
+    $files = glob($folder . "/*");
+    
+    foreach( $files as $file )
+    {
+        
+        if(is_dir($file))
+        {
+            loadPHP($file);
+        } 
+        else
+        {
+            if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+                $phps[] = $file;
+            }
+        }
+
+    }
+
+}
+
+loadPHP("/app/resto");
+loadPHP("/app/vendor");
+
+foreach ($phps as $php) {
+    opcache_compile_file($php);
+}
