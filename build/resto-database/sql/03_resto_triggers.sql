6dc07cf23963e1d19538ea9ea5339b4c152d4d04
jjrom <jerome.gasperi@gmail.com>
2021-06-23 17:18:12 +0200

    Update store_geometry_part function

--- build/resto-database/sql/03_resto_triggers.sql
+++ build/resto-database/sql/03_resto_triggers.sql
@@ -1,40 +1,36 @@
 --
--- resto geometry_part split
+-- Subdivide input geometry to geometry_part table
 --
-
--- 
--- On INSERT resto.feature THEN
---
---     1. Split resto.feature.geom into part
---     2. Store parts within resto.geometry_part
---
-CREATE OR REPLACE FUNCTION resto.store_geometry_part()
-RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION resto.store_geometry_part(id UUID, collection TEXT, geom GEOMETRY)
+RETURNS VOID AS $$
 DECLARE
     geo_type TEXT;
 BEGIN
 
     -- Do nothing if geom is not set
-    IF NEW.geom IS NULL THEN
-        RETURN NEW;
+    IF geom IS NULL THEN
+        RETURN;
     END IF;
 
     -- First get geometry type
-    geo_type := GeometryType(NEW.geom);
+    geo_type := GeometryType(geom);
     
     -- Do not touch (MULTI)POINT
     IF geo_type = 'POINT' OR geo_type = 'MULTIPOINT' THEN
-        INSERT INTO resto.geometry_part (id, collection, part_num, geom) VALUES (NEW.id, NEW.collection, 1, NEW.geom);
+        INSERT INTO resto.geometry_part (id, collection, part_num, geom) VALUES (id, collection, 1, geom);
     ELSE
         INSERT INTO resto.geometry_part
-            SELECT NEW.id, NEW.collection, ordinality AS part_num, sub AS geom
-            FROM ST_SubDivide(NEW.geom, 32) WITH ordinality AS sub;
+            SELECT id, collection, ordinality AS part_num, sub AS geom
+            FROM ST_SubDivide(geom, 32) WITH ordinality AS sub;
     END IF;
 
-    RETURN NEW;
+    RETURN;
 
 END
 $$ LANGUAGE plpgsql;
 
+-- 
+-- On INSERT on resto.feature THEN subdivide the input feature geometry and store it in geometry_part table
+--
 DROP TRIGGER IF EXISTS update_geometry_part ON resto.feature;
-CREATE TRIGGER update_geometry_part AFTER INSERT ON resto.feature FOR EACH ROW EXECUTE PROCEDURE resto.store_geometry_part();
+CREATE TRIGGER update_geometry_part AFTER INSERT ON resto.feature FOR EACH ROW EXECUTE PROCEDURE resto.store_geometry_part(NEW.id, NEW.collection, NEW.geom);
