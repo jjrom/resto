f7d98d56213b4b60324e72c2ae2c89d4c6e8b986
jrom <jerome.gasperi@gmail.com>
2026-05-19 11:29:46 +0200

    Mover to 10.0.6

--- app/resto/core/RestoConstants.php
+++ app/resto/core/RestoConstants.php
@@ -20,7 +20,7 @@ class RestoConstants
     // [IMPORTANT] Starting resto 7.x, default routes are defined in RestoRouter class
 
     // resto version
-    const VERSION = '10.0.5';
+    const VERSION = '10.0.6';
 
     /* ============================================================
      *              NEVER EVER TOUCH THESE VALUES
