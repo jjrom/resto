310506f8fa4eb680635f1bc97a1ade7a75c46e54
jrom <jerome.gasperi@gmail.com>
2026-05-22 15:13:15 +0200

    Update to 10.1.0

--- app/resto/core/RestoConstants.php
+++ app/resto/core/RestoConstants.php
@@ -20,7 +20,7 @@ class RestoConstants
     // [IMPORTANT] Starting resto 7.x, default routes are defined in RestoRouter class
 
     // resto version
-    const VERSION = '10.0.6';
+    const VERSION = '10.1.0';
 
     /* ============================================================
      *              NEVER EVER TOUCH THESE VALUES
